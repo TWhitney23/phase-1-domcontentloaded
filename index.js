af269198b8bccacf778c3178132a4ad073bd7265
Travelle Whitney <travellewhitney@gmail.com>
2025-08-13 16:44:07 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    const p = document.querySelector("p");
+    if (p) {
+      p.textContent = "This is really cool!";
+    }
+  });
